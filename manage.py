7ce7d2100668b41285a201552043ef5585e92383
hpitta <hpitta@students.cs.ubc.ca>
2025-02-24 16:28:08 -0500

    updated manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,5 @@
 from flask.cli import FlaskGroup
 from scrimmage import app, db
-import os
-
-# Set Flask configuration using environment variables
-os.environ['FLASK_RUN_HOST'] = '0.0.0.0'
-os.environ['FLASK_RUN_PORT'] = '8000'
-os.environ['FLASK_DEBUG'] = '1'  # Enable debug mode
 
 # Create FlaskGroup instance
 cli = FlaskGroup(app)
